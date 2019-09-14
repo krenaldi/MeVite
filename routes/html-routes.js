fb22e3f394fffce940a9e2a8d2f44746eafeba78
Garpken <gknafey@gmail.com>
2019-09-14 11:48:09 -0400

    got ejs to work with server.js

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -8,26 +8,26 @@ module.exports = function(app) {
 //
 
 
-  // app.get("/", function(req, res) {
-  //   // If the user already has an account send them to the members page
-  //   // if (req.user) {
-  //   //   res.redirect("/members");
-  //   // }
-  //   res.render('signup');
-  // });
+  app.get("/", function(req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("../public/views/members");
+    }
+    res.render('../public/views/signup');
+  });
 //
   app.get("/login", function(req, res) {
       // If the user already has an account send them to the members page
     if (req.user) {
-        res.redirect("/members");
+        res.redirect("../public/views/members");
       }
-      res.render('login');
+      res.render('../public/views/login');
     });
   //
     // Here we've add our isAuthenticated middleware to this route.
     // If a user who is not logged in tries to access this route they will be 
     //redirected to the signup page
     app.get("/members", isAuthenticated, function(req, res) {
-      res.render('memmbers');
+      res.render('../public/views/members');
     });
   };
