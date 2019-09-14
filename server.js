c155fbf92fe78c554bae051f0490355483269339
tahreen zeba <tahreenzeba1@gmail.com>
2019-09-14 10:29:38 -0400

    Passport authentication succesfull

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ var session = require('express-session');
 var bodyParser = require('body-parser');
 var path = require('path');
 var router = express.Router();
+// var env = require('dotenv').config({path: __dirname + '/.env'});
+var passport   = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
 
 
 // Sets up the Express App
@@ -28,7 +32,45 @@ app.use(express.json());
 
 //Middleware to process request
 //==============================================================
-app.use(bodyParser.urlencoded({extended: false}));
+//For BodyParser
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+// For Passport
+//==============================================================
+app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+
+// import and add passport as an argument
+//=============================================================
+var authRoute = require('./routes/auth.js')(app,passport);
+
+passport.use(new LocalStrategy(function(username, password, done) {
+    // database.signin(username, password, done);
+    if (username === 'admin' && password === 'admin') {
+      console.log('Signin');
+      done(null, { username: username });
+    } else {
+      done(null, false);
+    }
+  }));
+  
+  app.post('/signin', passport.authenticate('local', {
+    successRedirect: '/accessed',
+    failureRedirect: '/access'
+  }));
+
+  passport.serializeUser(function(user, done) {
+    done(null, user);
+  });
+  
+  passport.deserializeUser(function(user, done) {
+    done(null, user);
+  });
+
 
 // express-session
 //=============================================================
@@ -40,9 +82,33 @@ app.use(session({
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 
+//=============================================================
+app.get('/', function(req, res) {
+ 
+    res.send('Welcome to Passport with Sequelize');
+ 
+});
+ 
+//Models
+//=============================================================
+var models = require("./models"); 
+
+//load passport strategies
+//=============================================================
+require('./passport/passport.js')(passport, models.user);
+
 // Routes
 // =============================================================
 require("./routes/api_routes.js")(app);
+var authRoute = require('./routes/auth.js')(app, passport);
+
+//Sync Database
+// =============================================================
+models.sequelize.sync().then(function() {
+    console.log('Nice! Database looks fine')
+}).catch(function(err) {
+    console.log(err, "Something went wrong with the Database Update!")
+});
 
 // Starts the server to begin listening
 // =============================================================
