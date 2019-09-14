b115edea6db929e1a55359cda5811232f4197226
tahreen zeba <tahreenzeba1@gmail.com>
2019-09-14 11:13:13 -0400

    Clearing untracked files

---
+++ controllers/authcontroller.js
@@ -0,0 +1,12 @@
+var exports = module.exports = {}
+ 
+exports.signup = function(req, res) {
+ 
+    res.json('signup');
+ 
+}
+exports.signin = function(req, res) {
+ 
+    res.json('signin');
+ 
+}
