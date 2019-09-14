35d226d013dec1f8b154f87f0b86d3e114e4c84e
Kris Renaldi <krenaldi@comcast.net>
2019-09-14 00:45:11 -0400

    Updated html-routes.js file to render ejs files instead of using the sendFile to pull up html files

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -12,7 +12,7 @@ module.exports = function(app) {
     if (req.user) {
       res.redirect("/members");
     }
-    res.render('index');
+    res.render('signup');
   });
 //
   app.get("/login", function(req, res) {
@@ -20,13 +20,13 @@ module.exports = function(app) {
     if (req.user) {
         res.redirect("/members");
       }
-      res.sendFile(path.join(__dirname, "../public/login.html"));
+      res.render('login');
     });
   //
     // Here we've add our isAuthenticated middleware to this route.
     // If a user who is not logged in tries to access this route they will be 
     //redirected to the signup page
     app.get("/members", isAuthenticated, function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/members.html"));
+      res.render('memmbers');
     });
   };
