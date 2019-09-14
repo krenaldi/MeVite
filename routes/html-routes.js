2c00e1d0932260668e99fa8bc613b3768fb79019
Kris Renaldi <krenaldi@comcast.net>
2019-09-14 00:24:15 -0400

    Added home route to go to index page

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,12 +6,13 @@ var isAuthenticated = require("../config/middleware/isAuthenticated");
 //
 module.exports = function(app) {
 //
+
   app.get("/", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
       res.redirect("/members");
     }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
+    res.render('index');
   });
 //
   app.get("/login", function(req, res) {
