257fba9c08879b6d9b4d84bd6b172ca2c6266021
tahreen zeba <tahreenzeba1@gmail.com>
2019-09-19 21:26:05 -0400

    Create new post from event and contacts with sequelize

--- models/user.js
+++ models/user.js
@@ -4,14 +4,14 @@ var bcrypt = require("bcryptjs");
 //
 // Creating our User model
 //Set it as export because we will need it required on the server
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
   var User = sequelize.define("User", {
     // The email cannot be null, and must be a proper email before creation
-    firstName : {
+    firstName: {
       type: DataTypes.STRING,
       allowNull: false
     },
-    lastName : {
+    lastName: {
       type: DataTypes.STRING,
       allowNull: false
     },
@@ -23,29 +23,26 @@ module.exports = function(sequelize, DataTypes) {
         isEmail: true
       }
     },
-   // The password cannot be null
-   password: {
-    type: DataTypes.STRING,
-    allowNull: false
-  }
-});
-// Creating a custom method for our User model. 
-//This will check if an unhashed password entered by the 
-//user can be compared to the hashed password stored in our database
-User.prototype.validPassword = function(password) {
-  return bcrypt.compareSync(password, this.password);
-};
-// Hooks are automatic methods that run during various phases of the User Model lifecycle
-// In this case, before a User is created, we will automatically hash their password
-//=======================================================================================
-// User.hook("beforeCreate", function(user) {
-//   user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
-// });
-// return User;
-
-User.beforeCreate(user => {
-   user.password = bcrypt.hashSync(
-     user.password,
+    // The password cannot be null
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
+// one user can have many events
+User.associate = function(models){
+  User.hasMany(models.createEvent)
+}
+  // Creating a custom method for our User model. 
+  //This will check if an unhashed password entered by the 
+  //user can be compared to the hashed password stored in our database
+  User.prototype.validPassword = function (password) {
+    return bcrypt.compareSync(password, this.password);
+  };
+  
+  User.beforeCreate(user => {
+    user.password = bcrypt.hashSync(
+      user.password,
       bcrypt.genSaltSync(10),
       null
     );
