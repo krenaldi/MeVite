b1a0720916281bd8e3ab6d92a1a87e5dc3cd0134
Kris Renaldi <krenaldi@comcast.net>
2019-09-20 23:22:47 -0400

    Added two lines to createEvents.js file to require the contacts and user model files

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "passroot",
+    "password": "rootpass",
     "database": "mevite",
     "host": "localhost",
     "dialect": "mysql",
--- public/js/createEvent.js
+++ public/js/createEvent.js
@@ -11,9 +11,9 @@ $(document).ready(function () {
   var zipcode = $("#zipcode");
   var country = $("#country");
 
-  // When the signup button is clicked, we validate the email and password are not blank
+  // Trigger the create event function on submit
   createEvents.on("submit", function (event) {
-    event.stopImmediatePropagation();
+    event.preventDefault();
     var userData = {
       title: title.val().trim(),
       date: date.val().trim(),
@@ -29,13 +29,11 @@ $(document).ready(function () {
       !userData.city || !userData.state || !userData.zipcode || !userData.country) {
         return;
       }
-      // If we have an email and password, run the signUpUser function
+      // If we have all the userData, run the createNewEvent function
       createNewEvent(userData);
-      
   });
 
-  // Does a post to the createEvent route. If succesfull, we are redirected to the members page
-  // Otherwise we log any errors
+  // Does a post to the createEvent route. If succesful, we are redirected to the members page otherwise we log any errors
   function createNewEvent(userData) {
     $.post("/createEvent", userData).then(function (data) {
       console.log(data)
@@ -44,57 +42,43 @@ $(document).ready(function () {
     }).then(appendEventHistory()).catch(handleLoginErr);
   }
 
-  // function updateEvent(userData)
-
+  // function to throw error alert
   function handleLoginErr(err) {
     $("#alert .msg").text(err.responseJSON);
     $("#alert").fadeIn(500);
   }
 
-
   function appendEventHistory() {
       $("#eventDetails").empty();
       $.get("/createEvent", function(data) {
-
         if(data.length >= 0) {
-        console.log(data.length);
-        $("#eventDetails").append("<h2>Your Events!</h2>")
-        $("#eventDetails").append("____________________________________________________________________")
+        console.log(data);
+        $("#eventDetails").append("<h2>Your Events!</h2>");
         for(let i=0; i < data.length; i++) {
           $("#eventDetails").append("<div id='deleteMe' >");
           $("#eventDetails").append(`<p>Event Name: ${data[i].title} </p>`);
-          $("#eventDetails").append(`<p>Event Date: ${data[i].date} </p>`);
+          $("#eventDetails").append(`<p>Event Date: ${data[i].date} Time: ${data[i].time}</p>`);
           $("#eventDetails").append(`<p>Event Location: ${data[i].address}, ${data[i].city} ${data[i].state}, ${data[i].zipcode} ${data[i].country} </p>`);
-          $("#eventDetails").append(`<p>Event Name: ${data[i].createdAt} </p>`);
-          $("#eventDetails").append(`<p>Event Name: ${data[i].updatedAt} </p>`);
+          // $("#eventDetails").append(`<p>Event Name: ${data[i].createdAt} </p>`);
+          // $("#eventDetails").append(`<p>Event Name: ${data[i].updatedAt} </p>`);
           $("#eventDetails").append(`<button class="btn btn-default login-button">Edit</button>
           <button value="${data.title}" id="deleted" class="btn btn-default login-button">Delete</button>
           <button class="btn btn-default login-button">Send Invites</button> <br>`)
-          $("#eventDetails").append("__________________________________________________________________");
           $("#eventDetails").append("</div>");
         }
       }else{
         $("#eventDetails").append("<h2>You currently have no events planned</h2>");
       }
       });
-    
-   
   }
   
-  
- 
-  
-
-  $("#deleted").on("click", function(event2) {
-    event2.stopImmediatePropagation();
+  $("#deleted").on("click", function(event) {
+    event.preventDefaultn();
     var title = $(this).value();
-  
     $.delete(`/createEvent/:${title}`).then(function() {
       appendEventHistory();
     })
     
   });
-   
-  
 });
 
